e94201d4d8b291f7b0d8507aa917be9e7e2b28d1
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-11-25 20:51:31 -0200

    ✨ adding observable properties for monster and adventurer @MapArtifact

--- rpg/src/env/MapArtifact.java
+++ rpg/src/env/MapArtifact.java
@@ -4,19 +4,31 @@ import jason.asSyntax.Atom;
 import cartago.*;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+
 
 public class MapArtifact extends Artifact {
 
 	Map<String,Position> playersPosition = new HashMap<String,Position>();
+	Map<String,Position> monstersPosition = new HashMap<String,Position>();
 
 	public void init(){
 
 	}
 
-	@OPERATION public void enter_map(int h, int v){
+	@OPERATION
+	public void enter_map(int h, int v){
 		String agentName = getCurrentOpAgentId().getAgentName();
 		Position agentPos = new Position(h,v);
 		playersPosition.put(agentName, agentPos);
+		defineObsProperty("adventurer", agentName, h, v);
+	}
+
+	@OPERATION
+	public void add_monsters(String monsterName, int h, int v){
+		Position agentPos = new Position(h,v);
+		monstersPosition.put(monsterName, agentPos);
+		defineObsProperty("monster", monsterName, h, v);
 	}
 
 }
